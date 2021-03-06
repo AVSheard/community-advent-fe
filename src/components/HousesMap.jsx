8403d410591e5b9a48787785463efd7197fdcd5e
AVSheard <a_sheard@yahoo.co.uk>
2021-03-06 11:27:46 +0000

    got map to apear in app

--- src/components/HousesMap.jsx
+++ src/components/HousesMap.jsx
@@ -1,12 +1,26 @@
 import React, { Component } from "react";
 import L from "leaflet";
-import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 L.Icon.Default.imagePath = "https://unpkg.com/leaflet@1.5.0/dist/images/";
 
 class HousesMap extends Component {
 	render() {
-		return <div className="mapContainer"></div>;
+		return (
+			<div className="mapContainer">
+				<MapContainer
+					center={{
+						lat: 53.5382321,
+						lng: -1.9920182,
+					}}
+					zoom={15}>
+					<TileLayer
+						attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+						url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+					/>
+				</MapContainer>
+			</div>
+		);
 	}
 }
 
