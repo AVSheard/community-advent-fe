765fa17bf2df99be60fee0931ba7a5134632d379
AVSheard <a_sheard@yahoo.co.uk>
2021-03-08 11:38:20 +0000

    got popups to apear above markers

--- src/components/HousesMap.jsx
+++ src/components/HousesMap.jsx
@@ -6,6 +6,7 @@ L.Icon.Default.imagePath = "https://unpkg.com/leaflet@1.5.0/dist/images/";
 
 class HousesMap extends Component {
 	state = {
+		activeHouse: null,
 		houses: [
 			{
 				house_id: 1,
@@ -41,12 +42,31 @@ class HousesMap extends Component {
 						attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 						url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 					/>
+
 					{this.state.houses.map((house) => (
 						<Marker
 							key={house.house_id}
 							position={[house.houseLongLoc, house.houseLatLoc]}
+							eventHandlers={{
+								click: () => {
+									this.setState({ activeHouse: [house] });
+								},
+							}}
 						/>
 					))}
+
+					{this.state.activeHouse && (
+						<Popup
+							position={[
+								this.state.activeHouse[0].houseLongLoc,
+								this.state.activeHouse[0].houseLatLoc,
+							]}
+							onClose={() => this.setState({ activeHouse: null })}>
+							<div>
+								<h2>{this.state.activeHouse[0].day}</h2>
+							</div>
+						</Popup>
+					)}
 				</MapContainer>
 			</div>
 		);
