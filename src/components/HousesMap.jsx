40a9edd47e1f9b4c5b527da036d2606f96787bf4
AVSheard <a_sheard@yahoo.co.uk>
2021-04-21 16:51:08 +0100

    refactorred code for simplicity

--- src/components/HousesMap.jsx
+++ src/components/HousesMap.jsx
@@ -3,143 +3,10 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { Icon } from "leaflet";
 import houses from "../data/Uppermill";
 
-const numOne = new Icon({
-	iconUrl: "/numbers/num1.png",
-	iconSize: [25, 25],
-});
-let numIcons = [
-	new Icon({
-		iconUrl: "/numbers/num1.png",
-		iconSize: [25, 25],
-	}),
-	new Icon({
-		iconUrl: "/numbers/num1.png",
-		iconSize: [25, 25],
-	}),
-	new Icon({
-		iconUrl: "/numbers/num1.png",
-		iconSize: [25, 25],
-	}),
-	new Icon({
-		iconUrl: "/numbers/num1.png",
-		iconSize: [25, 25],
-	}),
-	new Icon({
-		iconUrl: "/numbers/num1.png",
-		iconSize: [25, 25],
-	}),
-	new Icon({
-		iconUrl: "/numbers/num1.png",
-		iconSize: [25, 25],
-	}),
-	new Icon({
-		iconUrl: "/numbers/num1.png",
-		iconSize: [25, 25],
-	}),
-	new Icon({
-		iconUrl: "/numbers/num1.png",
-		iconSize: [25, 25],
-	}),
-	new Icon({
-		iconUrl: "/numbers/num1.png",
-		iconSize: [25, 25],
-	}),
-	new Icon({
-		iconUrl: "/numbers/num1.png",
-		iconSize: [25, 25],
-	}),
-	new Icon({
-		iconUrl: "/numbers/num1.png",
-		iconSize: [25, 25],
-	}),
-	new Icon({
-		iconUrl: "/numbers/num1.png",
-		iconSize: [25, 25],
-	}),
-	new Icon({
-		iconUrl: "/numbers/num1.png",
-		iconSize: [25, 25],
-	}),
-	new Icon({
-		iconUrl: "/numbers/num1.png",
-		iconSize: [25, 25],
-	}),
-	new Icon({
-		iconUrl: "/numbers/num1.png",
-		iconSize: [25, 25],
-	}),
-	new Icon({
-		iconUrl: "/numbers/num1.png",
-		iconSize: [25, 25],
-	}),
-	new Icon({
-		iconUrl: "/numbers/num1.png",
-		iconSize: [25, 25],
-	}),
-	new Icon({
-		iconUrl: "/numbers/num1.png",
-		iconSize: [25, 25],
-	}),
-	new Icon({
-		iconUrl: "/numbers/num1.png",
-		iconSize: [25, 25],
-	}),
-	new Icon({
-		iconUrl: "/numbers/num1.png",
-		iconSize: [25, 25],
-	}),
-	new Icon({
-		iconUrl: "/numbers/num1.png",
-		iconSize: [25, 25],
-	}),
-	new Icon({
-		iconUrl: "/numbers/num1.png",
-		iconSize: [25, 25],
-	}),
-	new Icon({
-		iconUrl: "/numbers/num1.png",
-		iconSize: [25, 25],
-	}),
-	new Icon({
-		iconUrl: "/numbers/num1.png",
-		iconSize: [25, 25],
-	}),
-	new Icon({
-		iconUrl: "/numbers/num1.png",
-		iconSize: [25, 25],
-	}),
-	new Icon({
-		iconUrl: "/numbers/num1.png",
-		iconSize: [25, 25],
-	}),
-	new Icon({
-		iconUrl: "/numbers/num1.png",
-		iconSize: [25, 25],
-	}),
-];
-
 class HousesMap extends Component {
 	state = {
 		activeHouse: null,
 		houses: houses,
-	};
-
-	handleError = (err) => {
-		this.setState({
-			msg: err.response.data.msg,
-			status: err.response.data.status,
-			loading: false,
-		});
-	};
-
-	componentDidMount = () => {
-		for (let i = 0; i < 25; i++) {
-			numIcons[i] = new Icon({
-				iconUrl: `/numbers/num${i + 1}.png`,
-				iconSize: [25, 25],
-			});
-		}
-		this.forceUpdate();
 	};
 
 	render() {
@@ -167,7 +34,12 @@ class HousesMap extends Component {
 											this.setState({ activeHouse: [house] });
 										},
 									}}
-									icon={numIcons[house.day - 1]}
+									icon={
+										new Icon({
+											iconUrl: `/numbers/num${house.day}.png`,
+											iconSize: [25, 25],
+										})
+									}
 								/>
 							)
 						);
